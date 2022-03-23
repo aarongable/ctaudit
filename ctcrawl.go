3c86b0a627c10e5975db0d1ed7534ce931b89fd1
Aaron Gable <aaron@aarongable.com>
2022-03-23 14:45:00 -0700

    Use fetcher lib for better retries

--- ctcrawl.go
+++ ctcrawl.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	ct "github.com/google/certificate-transparency-go"
 	"github.com/google/certificate-transparency-go/client"
 	"github.com/google/certificate-transparency-go/jsonclient"
+	"github.com/google/certificate-transparency-go/scanner"
 )
 
 // entryData contains the index / sequence number of an entry, as well as the
@@ -19,7 +23,7 @@ import (
 // so that it can be used in a Heap.
 type entryData struct {
 	index int64
-	entry ct.LogEntry
+	entry *ct.RawLogEntry
 }
 
 func (ed entryData) Before(other entryData) bool {
@@ -27,22 +31,32 @@ func (ed entryData) Before(other entryData) bool {
 }
 
 func processEntry(prev, curr entryData) {
-	if prev.entry.Leaf.TimestampedEntry.Timestamp > 10+curr.entry.Leaf.TimestampedEntry.Timestamp {
+	if prev.entry.Leaf.TimestampedEntry.Timestamp > 1000+curr.entry.Leaf.TimestampedEntry.Timestamp {
 		fmt.Println("Found out-of-order entry:")
-		fmt.Printf("Index: %d\n", prev.index)
-		fmt.Printf("Timestamps: %d, %d\n", prev.entry.Leaf.TimestampedEntry.Timestamp, curr.entry.Leaf.TimestampedEntry.Timestamp)
-		if prev.entry.X509Cert != nil {
-			fmt.Printf("Serial: %d\n", prev.entry.X509Cert.SerialNumber)
-		} else {
-			fmt.Printf("Serial: %d\n", prev.entry.Precert.TBSCertificate.SerialNumber)
+		fmt.Printf("  Index: %d\n", prev.index)
+		fmt.Printf("  Timestamps: %d, %d\n", prev.entry.Leaf.TimestampedEntry.Timestamp, curr.entry.Leaf.TimestampedEntry.Timestamp)
+		switch prev.entry.Leaf.TimestampedEntry.EntryType {
+		case ct.X509LogEntryType:
+			cert, err := prev.entry.Leaf.X509Certificate()
+			if err != nil {
+				fmt.Printf("  Failed to parse: %v\n", err)
+			} else {
+				fmt.Printf("  Serial: %d\n", cert.SerialNumber)
+			}
+		case ct.PrecertLogEntryType:
+			cert, err := prev.entry.Leaf.Precertificate()
+			if err != nil {
+				fmt.Printf("  Failed to parse: %v\n", err)
+			} else {
+				fmt.Printf("  Serial: %d\n", cert.SerialNumber)
+			}
 		}
-		fmt.Println()
 	}
 }
 
 func main() {
 	logURI := flag.String("log_uri", "https://oak.ct.letsencrypt.org/2022/", "CT log base URI")
-	batchSize := flag.Int64("batch_size", 256, "Max number of entries to request at per call to get-entries")
+	batchSize := flag.Int("batch_size", 256, "Max number of entries to request at per call to get-entries")
 	numWorkers := flag.Int("num_workers", 2, "Number of concurrent workers")
 	startIndex := flag.Int64("start_index", 0, "Log index to start scanning at")
 	endIndex := flag.Int64("end_index", 0, "Log index to end scanning at (non-inclusive, 0 = end of log)")
@@ -64,49 +78,30 @@ func main() {
 		log.Fatal("Failed to create log client")
 	}
 
-	// Create a channel that just counts up from zero to the total number of
-	// batches that we'll have to request to cover the whole index space.
-	batchChan := make(chan int64)
-	go func() {
-		for i := int64(0); i <= (*endIndex-*startIndex)/(*batchSize); i++ {
-			batchChan <- i
-		}
-		close(batchChan)
-	}()
+	fetcher := scanner.NewFetcher(
+		logClient,
+		&scanner.FetcherOptions{
+			BatchSize:     *batchSize,
+			StartIndex:    *startIndex,
+			EndIndex:      *endIndex,
+			ParallelFetch: *numWorkers,
+			Continuous:    false,
+		},
+	)
 
-	// Create a collection of channels that all fetch batches of entries from the
-	// log, parse the leaf data (but not the actual certificates) in each entry,
-	// and send that data to an output channel.
-	var wg sync.WaitGroup
-	entryChan := make(chan entryData)
-	for w := 0; w < *numWorkers; w++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for b := range batchChan {
-				start := *startIndex + (b * (*batchSize))
-				end := *startIndex + ((b+1)*(*batchSize) - 1)
-
-				ctx := context.Background()
-				entries, err := logClient.GetEntries(ctx, start, end)
-				if err != nil {
-					return
-				}
-
-				for i, entry := range entries {
-					entryChan <- entryData{start + int64(i), entry}
-				}
+	// Create a callback that sends fetched entries to a channel to be processed.
+	entries := make(chan entryData)
+	processBatch := func(batch scanner.EntryBatch) {
+		for i, e := range batch.Entries {
+			index := batch.Start + int64(i)
+			rawLogEntry, err := ct.RawLogEntryFromLeaf(index, &e)
+			if err != nil {
+				fmt.Printf("failed to process entry at index %d: %v\n", index, err)
+				continue
 			}
-		}()
+			entries <- entryData{index: batch.Start + int64(i), entry: rawLogEntry}
+		}
 	}
-
-	// Create a goroutine which waits for all of the fetchers to finish their work
-	// and then closes the channel they were writing to, so that the processing
-	// goroutine also knows when to quit.
-	go func() {
-		wg.Wait()
-		close(entryChan)
-	}()
 
 	// Start a worker which reads entries from the fetchers. If it receives an
 	// entry for which it has not already processed the immediately preceding
@@ -118,32 +113,66 @@ func main() {
 	buffer := NewHeap[entryData]()
 	nextIndex := *startIndex
 	last := entryData{index: -1}
-	for e := range entryChan {
-		// If it's not the entry we want, save it for later.
-		if e.index != nextIndex {
-			buffer.Push(e)
-			continue
-		}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for e := range entries {
+			// If it's not the entry we want, save it for later.
+			if e.index != nextIndex {
+				buffer.Push(e)
+				continue
+			}
 
-		// If this is the first entry we're looking for, initialize our last-seen
-		// tracker so we have a basis for comparison.
-		if last.index == -1 {
-			last = e
-			nextIndex = e.index + 1
-			continue
-		}
+			// If this is the first entry we're looking for, initialize our last-seen
+			// tracker so we have a basis for comparison.
+			if last.index == -1 {
+				last = e
+				nextIndex = e.index + 1
+				continue
+			}
 
-		// This is the next entry we were looking for. Process it.
-		processEntry(last, e)
-		last = e
-		nextIndex += 1
-
-		// Try to process the buffer, just in case we've caught up to it.
-		for buffer.Len() > 0 && buffer.Peek().index == nextIndex {
-			e = buffer.Pop()
+			// This is the next entry we were looking for. Process it.
 			processEntry(last, e)
 			last = e
 			nextIndex += 1
+			if nextIndex%1000 == 0 {
+				fmt.Printf("Processed up to index %d\n", nextIndex)
+			}
+
+			// Try to process the buffer, just in case we've caught up to it.
+			for buffer.Len() > 0 && buffer.Peek().index == nextIndex {
+				e = buffer.Pop()
+				processEntry(last, e)
+				last = e
+				nextIndex += 1
+				if nextIndex%1000 == 0 {
+					fmt.Printf("Processed up to index %d\n", nextIndex)
+				}
+			}
 		}
+	}()
+
+	// Set up a context and a signal-catcher to cancel the context so we can
+	// break out cleanly if we need to.
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGTERM)
+		signal.Notify(sigChan, syscall.SIGINT)
+		signal.Notify(sigChan, syscall.SIGHUP)
+
+		<-sigChan
+		cancel()
+
+		os.Exit(0)
+	}()
+
+	// Finally, run the fetcher, letting it feed data into the worker above.
+	err = fetcher.Run(ctx, processBatch)
+	close(entries)
+	wg.Wait()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
